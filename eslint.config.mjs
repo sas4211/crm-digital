68731cc0c4604b1a5a98af69a22573fd6318d897
AmmarCodes <65684587+AmmarSafdari@users.noreply.github.com>
2026-05-29 22:57:30 +0500

    Migrate ESLint config to ESLint 9, clean up, and fix environment ignoring

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+const eslintConfig = [
+  ...nextVitals,
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+];
+
+export default eslintConfig;
